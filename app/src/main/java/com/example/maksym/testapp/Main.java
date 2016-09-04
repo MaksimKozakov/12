c88199447f28b5a78c5e2a30a74ab3c07cd6b379
maksimkozakov <lestatm@rambler.ru>
2016-09-04 15:40:03 +0200

    test

---
+++ app/src/main/java/com/example/maksym/testapp/Main.java
@@ -0,0 +1,19 @@
+package com.example.maksym.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
